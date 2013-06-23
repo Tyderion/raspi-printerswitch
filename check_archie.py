a0db9024b57c3c3eac701db7f28df434e33b7a9e
Gabriel Nadler <nadlergabriel@gmail.com>
2013-06-23 12:33:19 +0200

    hardcoded IP

--- check_archie.py
+++ check_archie.py
@@ -11,20 +11,18 @@ GPIO.setup(PIN, GPIO.OUT)
 
 def turn_on_printer():
   global printer_on, PIN
-  print "Turning printer on"
   GPIO.output(PIN, GPIO.HIGH)
   printer_on = True
 
 def turn_off_printer():
   global printer_on, PIN
-  print "Turning Printer off"
   GPIO.output(PIN, GPIO.LOW)
   printer_on = False
 
 
 printer_on = GPIO.input(PIN)
 while (True):
-  response = os.system("ping -c1 -W1 " + "archie")
+  response = os.system("ping -c1 -W1 " + "192.168.1.183")
   if response == 0:
     if not printer_on:
       turn_on_printer()
